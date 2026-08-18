b396a5cf5475be1a7fe770559256d5b87bdfd12d
Lucas Gabriel <lukasgaby2016@gmail.com>
2026-08-18 15:05:37 -0300

    Add files via upload

---
+++ questao8.py
@@ -0,0 +1,14 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+operacao = input("Digite a operação (+, -, *, /): ")
+
+if operacao == "+":
+    print(num1 + num2)
+elif operacao == "-":
+    print(num1 - num2)
+elif operacao == "*":
+    print(num1 * num2)
+elif operacao == "/":
+    print(num1 / num2)
+else:
+    print("Operação inválida")
