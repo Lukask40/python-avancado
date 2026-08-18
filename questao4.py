dfc617ab58215dffa675686881212aaf7c7b13b1
Lucas Gabriel <lukasgaby2016@gmail.com>
2026-08-18 14:19:53 -0300

    Atualizar o questao4.py
    correção

--- questao4.py
+++ questao4.py
@@ -1,7 +1,9 @@
-a =1
-b = 3
+num1 = int(input("Digite o primeironúmero: "))
+num2 = int(input("Digite o primeironúmero: "))
 
-if (a>b):
-    print("A é maior que B.")
+if (num1>num2):
+    print(f"o primeiro número é maior que o segundo.")
+elif (num2>num1):
+    print("o segundo número é maior que o primeiro.")
 else:
-    print("A é menor que B.")
+    print("Os dois números são iguais!!.")
